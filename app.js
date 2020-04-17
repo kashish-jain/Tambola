0d9644d6b04b9cd63e6b90b48ef462fba90e17ed
Kashish Jain <kashish.emailme@gmail.com>
2020-04-16 21:21:28 -0400

    frontend socket client not working

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+const path = require("path");
 const express = require("express");
 const app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
-io.on('connection', function (socket) {
-    console.log("new connection");
-  });
+io.on("connection", (socket) => {
+  console.log("new connection");
+});
 
-// All files are served from build folder which gets generated 
+// All files are served from build folder which gets generated
 // when frontend code is built
-app.use(express.static("build"));
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/build/index.html");
-})
+app.use(express.static(path.join(__dirname + "/build")));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/build/index.html");
+});
 
 app.listen(3000, (req, res) => {
-    console.log("listening on port 3000");
-})
+  console.log("listening on port 3000");
+});
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+
+// import io from 'socket.io-client';
+
+// const socket = io("http://localhost:3000");
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
