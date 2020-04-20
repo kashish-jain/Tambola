8f756543993c1f96e6a5d9c60c31726843fef333
Kashish Jain <kashish.emailme@gmail.com>
2020-04-20 05:19:26 -0400

    host receiving player's tickets

--- frontend/src/components/Host.tsx
+++ frontend/src/components/Host.tsx
@@ -1,19 +1,49 @@
 import * as React from "react";
 import { Component } from "react";
 import Board from "./Board";
+import { BoxState } from "./Box";
+import Ticket from "./Ticket";
 
 interface HostProps {
   socket: any;
 }
 
-interface HostState {}
+interface HostState {
+  checkingTicket: boolean;
+}
 
 class Host extends Component<HostProps, HostState> {
+  ticketFromPlayer: Array<Array<Array<BoxState>>> | undefined;
+  winningCallFromPlayer: string | undefined;
   constructor(props: HostProps) {
     super(props);
+    this.state = { checkingTicket: false };
+  }
+
+  componentDidMount() {
+    this.props.socket.on(
+      "callWinforHost",
+      (callWinType: string, houses: Array<Array<Array<BoxState>>>) => {
+        this.winningCallFromPlayer = callWinType;
+        this.ticketFromPlayer = houses;
+        this.setState({ checkingTicket: true });
+      }
+    );
   }
   render() {
-    return <Board socket={this.props.socket} />;
+    let playerTicket = this.state.checkingTicket ? (
+      <div>
+        <br></br>
+        <Ticket houses={this.ticketFromPlayer} />
+        <p>Win Call: {this.winningCallFromPlayer}</p>
+      </div>
+    ) : null;
+    return (
+      <div>
+        <Board socket={this.props.socket} />
+        {playerTicket}
+      </div>
+    );
   }
 }
 
