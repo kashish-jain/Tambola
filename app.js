36871623220d1a184d0d1bae3623c92d52a86e6e
chirag1712 <chiragjindal1712@gmail.com>
2020-04-19 21:56:18 -0400

    Fixed spaces

--- app.js
+++ app.js
@@ -9,31 +9,31 @@ var io = require('socket.io').listen(server);
 var num_players = 0;
 
 io.on("connection", (socket) => {
-	
-	// logic for Host and PC connection
-	num_players++;
+  
+  // logic for Host and PC connection
+  num_players++;
   console.log(`new connection: ${num_players}`);
 
   // events emitted for new connection
   if(num_players == 1) {
-  	io.emit(`user connected`, { type: 'Host' });
+    io.emit(`user connected`, { type: 'Host' });
   } else {
-  	io.emit(`user connected`, { type: 'PC' });
+    io.emit(`user connected`, { type: 'PC' });
   }
 
   // events for host calling number from front-end button click
   socket.on('new_number', (number) => {
-  	// event for notifying PCs that new number was called
-  	socket.broadcast.emit(`new_number_from_host`, { new_number: number });
+    // event for notifying PCs that new number was called
+    socket.broadcast.emit(`new_number_from_host`, { new_number: number });
     console.log(`new_number: ${number}`);
   });
 
   // deal with disconnects here later
   // CASES:
-  //	- dealing with host's disconnection
+  //  - dealing with host's disconnection
   //  - dealing with PC's disconnection and joining back - use cookies I guess
-	socket.on('disconnect', () => {
-		num_players--;
+  socket.on('disconnect', () => {
+  num_players--;
     console.log('user disconnected');
   });
 });
