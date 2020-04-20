f72959092b9498318b6801110410d5afeb63a294
chirag1712 <chiragjindal1712@gmail.com>
2020-04-19 21:16:30 -0400

    Added connection and new_number events for backend

--- app.js
+++ app.js
@@ -7,8 +7,35 @@ const server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
 io.on("connection", (socket) => {
-  console.log("new connection");
+	
+	// logic for Host and PC connection
+	num_players++;
+  console.log(`new connection: ${num_players}`);
+
+  // events emitted for new connection
+  if(num_players == 1) {
+  	io.emit(`user connected`, { type: 'Host' });
+  } else {
+  	io.emit(`user connected`, { type: 'PC' });
+  }
+
+  // events for host calling number from front-end button click
+  socket.on('new_number', (number) => {
+  	// event for notifying PCs that new number was called
+  	socket.broadcast.emit(`new_number_from_host`, { new_number: number });
+    console.log(`new_number: ${number}`);
+  });
+
+  // deal with disconnects here later
+  // CASES:
+  //	- dealing with host's disconnection
+  //  - dealing with PC's disconnection and joining back - use cookies I guess
+	socket.on('disconnect', () => {
+		num_players--;
+    console.log('user disconnected');
+  });
 });
+
 
 // All files are served from build folder which gets generated
 // when frontend code is built
