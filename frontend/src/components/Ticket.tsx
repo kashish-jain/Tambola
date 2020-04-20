0266ff88846c8bdbd4111a0133e334ba08762008
Kashish Jain <kashish.emailme@gmail.com>
2020-04-19 23:11:41 -0400

    ticket component

---
+++ frontend/src/components/Ticket.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { Component } from "react";
+import House from "./House";
+import WinningButtons from "./WinningButtons";
+
+interface TicketProps {}
+
+interface TicketState {}
+
+class Ticket extends Component<TicketProps, TicketState> {
+  constructor(props: TicketProps) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <House key={0} />
+        <WinningButtons
+          key={0}
+          firstLine={"First Line"}
+          secondLine={"Second Line"}
+          thirdLine={"Third Line"}
+          corners={"Corners"}
+          fullHouse={"Full House"}
+        />
+      </div>
+    );
+  }
+}
+
+export default Ticket;
