89b29e1dc4672fa029e5dd19318ca7dfc4d3c5b5
kashish-jain <47945899+kashish-jain@users.noreply.github.com>
2020-04-21 17:21:35 -0400

    Chat prototype (#14)
    Basic chat component. Not much functionality but POC

--- frontend/src/components/Player.tsx
+++ frontend/src/components/Player.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { Component } from "react";
 import Ticket from "./Ticket";
 import NewNumber from "./NewNumber";
+import { Widget, addResponseMessage } from "react-chat-widget";
+import "react-chat-widget/lib/styles.css";
 
 interface PlayerProps {
   socket: any;
@@ -13,11 +15,24 @@ class Player extends Component<PlayerProps, PlayerState> {
   constructor(props: PlayerProps) {
     super(props);
   }
+
+  handleNewUserMessage = (newMessage: string) => {
+    console.log(`New message incoming! ${newMessage}`);
+    this.props.socket.emit("messageFromClient", newMessage);
+  };
+
+  componentDidMount() {
+    this.props.socket.on("messageToClient", (msg: string) => {
+      console.log("message received frontend ", msg);
+      addResponseMessage(msg);
+    });
+  }
   render() {
     return (
       <>
         <Ticket socket={this.props.socket} />
         <NewNumber socket={this.props.socket} />
+        <Widget handleNewUserMessage={this.handleNewUserMessage} />
       </>
     );
   }
