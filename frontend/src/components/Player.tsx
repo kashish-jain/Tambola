8d408475bf29da0d2a05c31f0f9f9116fbe12135
Kashish Jain <kashish.emailme@gmail.com>
2020-04-22 17:05:10 -0400

    removed chat

--- frontend/src/components/Player.tsx
+++ frontend/src/components/Player.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import { Component } from "react";
 import Ticket from "./Ticket";
 import NewNumber from "./NewNumber";
-import { Widget, addResponseMessage } from "react-chat-widget";
-import "react-chat-widget/lib/styles.css";
 
 interface PlayerProps {
   socket: any;
@@ -16,23 +14,11 @@ class Player extends Component<PlayerProps, PlayerState> {
     super(props);
   }
 
-  handleNewUserMessage = (newMessage: string) => {
-    console.log(`New message incoming! ${newMessage}`);
-    this.props.socket.emit("messageFromClient", newMessage);
-  };
-
-  componentDidMount() {
-    this.props.socket.on("messageToClient", (msg: string) => {
-      console.log("message received frontend ", msg);
-      addResponseMessage(msg);
-    });
-  }
   render() {
     return (
       <>
         <Ticket socket={this.props.socket} />
         <NewNumber socket={this.props.socket} />
-        <Widget handleNewUserMessage={this.handleNewUserMessage} />
       </>
     );
   }
