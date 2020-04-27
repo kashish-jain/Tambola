2b2e7295d3e6d3e306b004800f09bdf98a9cf79c
chirag1712 <chiragjindal1712@gmail.com>
2020-04-27 17:16:55 -0400

    updated prizes to keep track of who won what

--- frontend/src/components/Prizes.tsx
+++ frontend/src/components/Prizes.tsx
@@ -9,29 +9,50 @@ interface PrizesProps {
 }
 
 interface PrizesState {
+  // remaining awards should store, nameAward and numAward
   remainingAwards: Award[];
+
+  // also keeping track of whoWonWhat
+  // string -> string[]
+  // nameAward -> players who have won it
+  whoWonWhat: {
+    [nameAward: string]: string[];
+  };
 }
 
 class Prizes extends Component<PrizesProps, PrizesState> {
   constructor(props: PrizesProps) {
     super(props);
-    this.state = { remainingAwards: this.props.awards };
+    this.state = { remainingAwards: this.props.awards, whoWonWhat: {} };
   }
   componentDidMount() {
     this.props.socket.on("resultsForPC", (resultsObj: resultObj) => {
       console.log("result obj for updating available prizes", resultsObj);
       if (resultsObj.result == "Confirm Win!") {
         let currAwards = this.state.remainingAwards;
+        let currWhoWonWhat = this.state.whoWonWhat;
         for (let i = 0; i < currAwards.length; ++i) {
           if (currAwards[i].nameAward == resultsObj.callWinType) {
             // decrement currAwards[i].numAward
             let currNumAward = +currAwards[i].numAward;
             --currNumAward;
             currAwards[i].numAward = currNumAward.toString();
+
+            // update whoWonWhat
+            if (currWhoWonWhat[resultsObj.callWinType] === undefined) {
+              currWhoWonWhat[resultsObj.callWinType] = [
+                resultsObj.calledWinUsername,
+              ];
+            } else {
+              currWhoWonWhat[resultsObj.callWinType].push(
+                resultsObj.calledWinUsername
+              );
+            }
           }
         }
         this.setState({
           remainingAwards: currAwards,
+          whoWonWhat: currWhoWonWhat,
         });
       }
     });
@@ -44,6 +65,9 @@ class Prizes extends Component<PrizesProps, PrizesState> {
         <tr key={i}>
           <td>{this.state.remainingAwards[i].nameAward}</td>
           <td>x{this.state.remainingAwards[i].numAward}</td>
+          <td>
+            {this.state.whoWonWhat[this.state.remainingAwards[i].nameAward]}
+          </td>
         </tr>
       );
     }
