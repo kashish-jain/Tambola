d7dcc495d736ba9c4d6a5c6d87abab29e15a46f7
chirag1712 <chiragjindal1712@gmail.com>
2020-04-26 20:34:06 -0400

    Added available prizes functionality

--- frontend/src/components/Prizes.tsx
+++ frontend/src/components/Prizes.tsx
@@ -20,11 +20,41 @@ class Prizes extends Component<PrizesProps, PrizesState> {
   componentDidMount() {
     this.props.socket.on("resultsForPC", (resultsObj: resultObj) => {
       console.log("result obj for updating available prizes", resultsObj);
+      if (resultsObj.result == "Confirm Win!") {
+        let currAwards = this.state.remainingAwards;
+        for (let i = 0; i < currAwards.length; ++i) {
+          if (currAwards[i].nameAward == resultsObj.callWinType) {
+            // decrement currAwards[i].numAward
+            let currNumAward = +currAwards[i].numAward;
+            --currNumAward;
+            currAwards[i].numAward = currNumAward.toString();
+          }
+        }
+        this.setState({
+          remainingAwards: currAwards,
+        });
+      }
     });
   }
   render() {
     // use state.remainingAwards to make a table
-    return <></>;
+    let prizeComp = [];
+    for (let i = 0; i < this.state.remainingAwards.length; ++i) {
+      prizeComp.push(
+        <tr key={i}>
+          <td>{this.state.remainingAwards[i].nameAward}</td>
+          <td>x{this.state.remainingAwards[i].numAward}</td>
+        </tr>
+      );
+    }
+    return (
+      <table className="ready-player-container">
+        <tr>
+          <th colSpan={2}>Prizes</th>
+        </tr>
+        {prizeComp}
+      </table>
+    );
   }
 }
 
