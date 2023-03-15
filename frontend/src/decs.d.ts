1ff7a0d8fe54e0a2411271e69963be08df22585e
Kashish <kashish.emailme@gmail.com>
2023-03-15 21:14:08 +0000

    added comment

--- frontend/src/decs.d.ts
+++ frontend/src/decs.d.ts
@@ -1,3 +1,7 @@
 // This file is for declaring modules which do not have type declaration files
 declare module "react-rewards";
-declare module "socket.io-client"
+
+// this is a hack. On render, when we build we get an error that this module's types aren't 
+// available, so declaring it here. But it has something to do with eslintconfig
+// Relevant: https://github.com/facebook/create-react-app/issues/9791
+declare module "socket.io-client" 
