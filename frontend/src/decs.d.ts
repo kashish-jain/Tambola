bdaccc2f08a7c23efcf67fa2b395f78d1c89bc1d
Kashish <47945899+kashish-jain@users.noreply.github.com>
2023-03-15 17:17:27 -0400

    Render for deployment (#94)
    using yarn for package management and successfully deployed on render

--- frontend/src/decs.d.ts
+++ frontend/src/decs.d.ts
@@ -1,2 +1,7 @@
 // This file is for declaring modules which do not have type declaration files
 declare module "react-rewards";
+
+// this is a hack. On render, when we build we get an error that this module's types aren't 
+// available, so declaring it here. But it has something to do with eslintconfig
+// Relevant: https://github.com/facebook/create-react-app/issues/9791
+declare module "socket.io-client" 
